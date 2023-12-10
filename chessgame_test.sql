38fc08f79fd62111659dc6abb671cd05d68da912
Liamsi4657 <jeq.ismail@gmail.com>
2023-12-10 17:37:14 +0100

    Change chessgame_test

--- chessgame_test.sql
+++ chessgame_test.sql
@@ -11,6 +11,28 @@ INSERT INTO t values (5, '1. e4 e5'),(6, '1. e4 d5 2. exd5 Qxd 3. Nc3 Qd8 4. Bc4
 select g as g, getFirstMoves(g, 3) as first_moves, hasOpening(g, '1. d5') as openingwrong, hasOpening(g, '1. e4') as openingright from t;
 select * from t;
 
+-- Step 1: Create a function to generate random chess games
+CREATE OR REPLACE FUNCTION random_game() RETURNS text AS $$
+DECLARE
+  moves text[] := ARRAY['1. e4', '1. d4', '1. Nf3', '1. c4'];
+BEGIN
+  RETURN moves[1+random()*(array_length(moves, 1)-1)::int] || ' e5 2. Nf3 Nc6 3. Bb5';
+END;
+$$ LANGUAGE plpgsql;
+
+-- Step 2: Insert a large number of random games into your table
+DO $$
+BEGIN
+  FOR i IN 1..200 LOOP
+    INSERT INTO t (g) VALUES (random_game());
+  END LOOP;
+END;
+$$;
+
+SELECT COUNT(*) FROM t;
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM t WHERE hasOpening(g, '1. e4');
+
 create index t_idx on t(g);
 
-EXPLAIN ANALYZE SELECT COUNT(*) FROM t WHERE hasOpening(g, '1. e3');
+EXPLAIN ANALYZE SELECT COUNT(*) FROM t WHERE hasOpening(g, '1. e4');
